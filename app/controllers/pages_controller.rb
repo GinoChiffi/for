f8413f7625d88c83c9db6b9f83409ade19dca4ed
Gino Chiffi <gino.chiffi@gmail.com>
2017-04-16 15:19:55 +0200

    Landing and login page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def landing
+  end
+
+  def search
+  end
+
+  def login
+  end
+
+  def signup
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#landing'
+  get 'search' => 'pages#search'
+  get 'login' => 'pages#login'
+  get 'signup' => 'pages#signup'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
