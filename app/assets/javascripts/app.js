006f7432af7599f4f85e0bed60fca0986bc9edc8
Gino Chiffi <gino.chiffi@gmail.com>
2017-04-18 14:08:11 +0200

    Owlcar js

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+$(".owlcarousel").owlCarousel({
+
+navigation: true, // Show next and prev buttons
+slideSpeed: 300,
+paginationSpeed: 400,
+singleItem: false
+
+
+});
+
+});
